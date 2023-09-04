4fe38566ab19643b92353b368eac42777e5e954b
karel trisnanto utomo <kareltrisnanto26@gmail.com>
2023-09-04 23:32:57 +0700

    edit: footer email

--- src/components/FooterSection.vue
+++ src/components/FooterSection.vue
@@ -73,16 +73,16 @@
             <div>
               <h3 class="text-gray-700 uppercase font-semibold text-2xl">Hubungi Kami</h3>
               <a
-                href="https://wa.me/087791921967"
+                href="https://wa.me/082324282477"
                 target="_blank"
                 class="flex items-center gap-3 mt-2 hover:text-sky-600 text-sm text-gray-600"
                 ><v-icon name="hi-phone" class="text-cyan-700" scale="1.5" /> +62 823-2428-2477</a
               >
               <a
-                href="mailto:baikmu@gmail.com"
+                href="mailto:appkesehatanmental@gmail.com"
                 target="_blank"
                 class="flex items-center gap-3 mt-2 hover:text-sky-600 text-sm text-gray-600"
-                ><v-icon name="hi-mail" class="text-cyan-700" scale="1.5" /> appkesehatanmental@email.com</a
+                ><v-icon name="hi-mail" class="text-cyan-700" scale="1.5" /> appkesehatanmental@gmail.com</a
               >
               <a
                 href="https://goo.gl/maps/uex2bEBQnxSmTZtc6"
